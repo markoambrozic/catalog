e3f307a9d9c00cda21842c2a86218a883d3dcf70
Marko Ambrožič <marko.ambrozic@studio-moderna.com>
2018-12-28 20:35:08 +0100

    debug travis deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,4 +5,5 @@ echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/
 sudo apt-get update
 sudo apt-get install -y kubectl
 export KUBECONFIG=$(pwd)/conf/kube-config-mil01-RSO.yml
+echo $KUBECONFIG
 kubectl get pods
