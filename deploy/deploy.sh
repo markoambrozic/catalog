a0a8423bbc2a23988296ff5f205b2243434da099
Marko Ambrožič <marko.ambrozic@studio-moderna.com>
2018-12-29 10:08:59 +0100

    Moved to production

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,5 +4,5 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
-export KUBECONFIG=$(pwd)/conf/kube-config-mil01-RSO.yml
+export KUBECONFIG=$(pwd)/conf/kube-config-mil01-RSO-production.yml
 kubectl apply -f app.yml
